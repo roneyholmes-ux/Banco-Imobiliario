192db3349432b8b2ce968c42166daef542d37e0f
roneyholmes-ux <roneyholmes@gmail.com>
2026-07-29 14:33:00 -0300

    Refactor Actions object to handle received actions
    Removed specific action methods and added a generic handleAction method for processing received actions.

--- js/actions.js
+++ js/actions.js
@@ -1,18 +1,12 @@
-// actions.js - Gatilhos de Ações do Usuário
+/**
+ * actions.js
+ * Central de despachos de ações.
+ */ 
 
 const Actions = {
-    // Chamado pelo clique do botão "Jogar Dado"
-    rollDice() {
-        Multiplayer.sendActionToHost('ROLL_DICE');
-    },
-
-    // Chamado pelo clique do botão "Comprar Propriedade"
-    buyProperty(propertyId) {
-        Multiplayer.sendActionToHost('BUY_PROPERTY', { propertyId: propertyId });
-    },
-
-    // Chamado pelo clique do botão "Passar Vez"
-    endTurn() {
-        Multiplayer.sendActionToHost('END_TURN');
+    handleAction(message) {
+        console.log("[Actions] Ação recebida:", message);
     }
 };
+
+window.Actions = Actions;
